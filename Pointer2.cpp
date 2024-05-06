5918d469025be4d70f10308d1b44f086779d60a5
asyiraafnufail <asyiraaf4@gmail.com>
2024-05-06 15:03:18 +0700

    menuliskan int main program

--- Pointer2.cpp
+++ Pointer2.cpp
@@ -9,3 +9,11 @@ class mahasiswa{
     }
 };
 
+int main(){
+    mahasiswa *mhs = new mahasiswa{1}; //pointer object mahasiswa
+    mhs->nim = 2;
+    mhs->showNim();
+    delete mhs;
+
+    return 0;
+}
