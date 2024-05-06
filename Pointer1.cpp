544c12057438419c8aa6f05f31cc9faaaa01edfe
asyiraafnufail <asyiraaf4@gmail.com>
2024-05-06 14:55:37 +0700

    menuliskan pointer dereferene

--- Pointer1.cpp
+++ Pointer1.cpp
@@ -16,4 +16,9 @@ int main(){
     mahasiswa &refMhs = mhs; //pointer reference refMhs
     refMhs.nim = 2; //member access operator
     mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; //pointer dereference
+    pMhs->nim = 3; //arrow operator
+    pMhs->showNim();
+    
 }
