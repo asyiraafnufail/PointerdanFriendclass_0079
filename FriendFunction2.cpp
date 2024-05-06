8c6334ec8ce69982ac77e73b4f4f059d9bceb336
asyiraafnufail <asyiraaf4@gmail.com>
2024-05-06 15:36:16 +0700

    menuliskan int main program'

--- FriendFunction2.cpp
+++ FriendFunction2.cpp
@@ -19,3 +19,11 @@ class pelajar{
 void manusia::showNilaiPelajar(pelajar &x){
     std::cout << x.nilai;
 }
+
+int main(){
+    manusia budi;
+    pelajar pbudi;
+    budi.showNilaiPelajar(pbudi);
+
+    return 0;
+}
